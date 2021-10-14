e9674a02bee0ec6761e3d17f28813ca5b772b667
Charles H J  Pearce <charleshjpearce@Charless-MacBook-Pro.local>
2021-10-14 17:45:19 +0100

    finished minigrep

---
+++ src/lib.rs
@@ -0,0 +1,91 @@
+use std::fs;
+use std::error::Error;
+use std::env;
+
+pub struct Config<'a> {
+    pub query: &'a str,
+    pub filename: &'a str,
+    pub case_sensitive: bool
+}
+
+impl<'a> Config<'a> {
+    pub fn new(args: &'a Vec<String>) -> Result<Config, &'static str> {
+
+        if args.len() < 3 {
+            // Returns the error which is much easier to read than backtracy type stuff
+            return Err("Not enough arguments")
+        }
+
+        let query = &args[1];
+        let filename = &args[2];
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+        
+        return Ok(Config{query, filename, case_sensitive})
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str, case_sensitive:bool) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    
+    if case_sensitive {
+        for line in contents.lines() {
+            if line.contains(query) {
+                results.push(line);
+            }
+        }
+    } else {
+        let query_lower = query.to_lowercase();
+        for line in contents.lines() {
+            if line.to_lowercase().contains(&query_lower) {
+                results.push(line);
+            }
+        }
+    }
+
+    return results
+}
+
+pub fn run(config:Config) -> Result<(),Box<dyn Error>> {
+    // the ? operator will propogate the error back
+    let contents = fs::read_to_string(config.filename)?;
+    let results = search(config.query, &contents, config.case_sensitive);
+
+    for result in results {
+        println!("{}",result);
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+        Rust:
+safe, fast, productive.
+Pick three.";
+        
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query,contents,true)
+        )
+    }
+    #[test]
+    
+    fn case_insensitive() {
+        let query = "Rust";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        
+assert_eq!(
+            vec!["Rust:"],
+            search(query,contents,false)
+        )
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+use std::process;
+use rust_minigrep::Config;
+
 fn main() {
-    println!("Hello, world!");
+    // Collect turns the iterator into a vec
+    let args: Vec<String> = env::args().collect();
+
+    let config : Config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem Parsing Arguments: {}",err);
+        process::exit(1);
+    });
+
+    if let Err(e) = rust_minigrep::run(config) {
+        println!("{}",e);
+        process::exit(1);
+    }
 }
